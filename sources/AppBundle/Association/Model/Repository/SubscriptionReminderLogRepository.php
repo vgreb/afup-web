ca18dfd0436e24284bbab2995a4d10852b0e30d9
vgreb <vgreb@users.noreply.github.com>
2026-07-27 09:50:01 +0200

    Découplage de SubscriptionReminderLogRepository vis-à-vis de CompanyMember
    Même problème que UserRepository : la liste des relances de cotisation
    (page admin) reposait sur l'auto-jointure Ting via l'alias "apm" pour
    afficher le nom de la personne morale associée à une relance. On
    retire la jointure de la requête et on résout la personne morale via
    PersonneMoraleRepository dans le contrôleur.

--- sources/AppBundle/Association/Model/Repository/SubscriptionReminderLogRepository.php
+++ sources/AppBundle/Association/Model/Repository/SubscriptionReminderLogRepository.php
@@ -19,10 +19,9 @@ class SubscriptionReminderLogRepository extends Repository implements MetadataIn
     {
         return $this->getQuery('
             SELECT asrl.id, asrl.user_id, asrl.user_type, asrl.email, asrl.reminder_key, asrl.reminder_date, asrl.mail_sent,
-            app.id, apm.id, app.nom, apm.nom, app.prenom, apm.prenom, apm.raison_sociale
+            app.id, app.nom, app.prenom, app.id_personne_morale
             FROM `afup_subscription_reminder_log` asrl
             LEFT JOIN afup_personnes_physiques app ON app.id = asrl.user_id
-            LEFT JOIN afup_personnes_morales apm ON apm.id = app.id_personne_morale
             ORDER BY asrl.reminder_date DESC
             LIMIT ' . (((int) $page - 1) * $limit) . ', ' . (int) ($limit + 1),
         )->query();
--- sources/AppBundle/Controller/Admin/Membership/ReminderLogAction.php
+++ sources/AppBundle/Controller/Admin/Membership/ReminderLogAction.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace AppBundle\Controller\Admin\Membership;
 
+use AppBundle\Association\Entity\Repository\PersonneMoraleRepository;
 use AppBundle\Association\Model\Repository\SubscriptionReminderLogRepository;
+use AppBundle\Association\Model\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
@@ -13,6 +15,7 @@ class ReminderLogAction
 {
     public function __construct(
         private readonly SubscriptionReminderLogRepository $subscriptionReminderLogRepository,
+        private readonly PersonneMoraleRepository $personneMoraleRepository,
         private readonly Environment $twig,
     ) {}
 
@@ -21,8 +24,20 @@ class ReminderLogAction
         $page = $request->attributes->getInt('page', 1);
         $limit = 50;
 
+        $logs = $this->subscriptionReminderLogRepository->getPaginatedLogs($page, $limit);
+
+        /** @var iterable<array{app: ?User}> $logs */
+        $companies = [];
+        foreach ($logs as $log) {
+            $companyId = $log['app']?->getCompanyId();
+            if ($companyId && !isset($companies[$companyId])) {
+                $companies[$companyId] = $this->personneMoraleRepository->find($companyId);
+            }
+        }
+
         return new Response($this->twig->render('admin/relances/liste.html.twig', [
-            'logs' => $this->subscriptionReminderLogRepository->getPaginatedLogs($page, $limit),
+            'logs' => $logs,
+            'companies' => $companies,
             'limit' => $limit,
             'page' => $page,
             'title' => 'Relances',
